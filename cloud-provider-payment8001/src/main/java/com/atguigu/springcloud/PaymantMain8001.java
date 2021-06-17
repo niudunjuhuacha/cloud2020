c1fde4dad8bd35d70ffbc458cd9adea5c7f000ba
niudunjuhuacha <623050427@qq.com>
2021-06-17 17:39:58 +0800

    在公司的配置

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymantMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymantMain8001.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+/**
+ * @EnableDiscoveryClient和@EnableEurekaClient
+ * 共同点就是：都是能够让注册中心能够发现，扫描到改服务。
+ * 不同点：@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。
+ */
+@EnableDiscoveryClient
 public class PaymantMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymantMain8001.class, args);
